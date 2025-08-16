25087e100c69329c9ef0776cc900bd36e791ea5e
Nirjus <karmakarnirjus4839@gmail.com>
2025-08-16 21:48:13 +0530

    complete CRUD in postgresSQL

--- src/concepts/basic-queries.js
+++ src/concepts/basic-queries.js
@@ -33,3 +33,58 @@ export async function insertUser(username, email) {
     console.log("[ERROR] error in inserting user");
   }
 }
+
+export async function getAllUsers() {
+  const getAllUsersQuery = `
+    SELECT * FROM users
+  `;
+  try {
+    const result = await query(getAllUsersQuery);
+    console.log("All users fetched from users table");
+    return result.rows;
+  } catch (error) {
+    console.log("[ERROR] error in fetching all users");
+  }
+}
+
+export async function updateUserEmail(username, email) {
+  const updateUserQuery = `
+    UPDATE users 
+    SET email = $2
+    WHERE username = $1
+    RETURNING *
+  `;
+  try {
+    const result = await query(updateUserQuery, [username, email]);
+
+    if (result.rows.length > 0) {
+      console.log("User updated successfull!", result.rows[0]);
+      return result.rows[0];
+    } else {
+      console.log("No user found");
+      return null;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function deleteUser(username) {
+  const deleteQuery = `
+      DELETE FROM users 
+      WHERE username = $1
+      RETURNING * 
+    `;
+  try {
+    const result = await query(deleteQuery, [username]);
+    if (result.rows.length > 0) {
+      console.log("User deleted sucessfully", result.rows[0]);
+      return result.rows[0];
+    } else {
+      console.log("No user found");
+      return null;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { createUsersTable, insertUser } from "./concepts/basic-queries.js";
+import {
+  createUsersTable,
+  deleteUser,
+  getAllUsers,
+  insertUser,
+  updateUserEmail,
+} from "./concepts/basic-queries.js";
 
 async function testBasicQuery() {
   try {
@@ -6,6 +12,15 @@ async function testBasicQuery() {
     // await insertUser("Naman khel", "naman@gmail.com");
     // await insertUser("Bhusan jadav", "bhusan@gmail.com");
     // await insertUser("Kriti tripathi", "tripathikriti@gmail.com");
+    // const users = await getAllUsers();
+    // console.log(users);
+    // const updatedUser = await updateUserEmail(
+    //   "Bhusan jadav",
+    //   "jadavBhusan123@gmail.com"
+    // );
+    // console.log(updatedUser);
+    const deletedUser = await deleteUser("Naman khel");
+    console.log(deletedUser);
   } catch (error) {
     console.log(error);
   }
